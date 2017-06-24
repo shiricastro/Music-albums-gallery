38a697701be3ddd16c42878aa7e1915bd71bebbd
shiri castro <shiricastro3@gmail.com>
2017-06-24 18:47:46 +0300

    add letter by letter text appearance with jquery

--- script.js
+++ script.js
@@ -6,17 +6,42 @@ function bildAlbums(data){
     var albume = $('div.albumContainer');
     $.each(data,function(i, val){
        var pic = $('<picture>');
+       $('<figcaption>',{class:"up"}).appendTo(pic);
        $('<img>', {src:val.img, alt:val.name, "data-name":val.name, "click":add}).appendTo(pic);
-       var name = $('<figcaption>',{text:val.name}).appendTo(pic);
+       $('<figcaption>',{text:val.name, class:"down"}).appendTo(pic);
        pic.appendTo(albume);
     });
 }
 
-function add(e){
-    if($(e.target).siblings('figcaption').hasClass('move')){
-        $(e.target).siblings('figcaption').removeClass('move');
-        $(e.target).siblings('figcaption').addClass('return'); 
+function add(e){   
+    let albumName = $(e.target).attr('data-name');
+    var timeOut;
+    let character = 0;
+    
+    if($(e.target).siblings('figcaption.up').text()=== ""){
+        move("up","down");
     }else{
-        $(e.target).siblings('figcaption').addClass('move'); 
+        move("down","up");
     }
+   
+    function move(one,two) {
+        timeOut = setTimeout(function() {
+            character++;
+            var type = albumName.substring(0, character);
+            $(e.target).siblings('figcaption.'+one + '').text(type);
+            var down = albumName.substring(character,albumName.length);
+            $(e.target).siblings('figcaption.'+two+ '').text(down);
+            move(one,two );
+            if (character === albumName.length) {
+                clearTimeout(timeOut);
+            }        
+        }, 200);
+    };
+
 }
+
+
+    
+
+
+  
